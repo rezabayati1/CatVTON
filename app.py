6a8bcb8ea7de67c512175e5579139e26597b9308
reza bayati <55228392+rezabayati1@users.noreply.github.com>
2024-08-30 12:49:54 +0330

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def parse_args():
     parser.add_argument(
         "--base_model_path",
         type=str,
-        default="runwayml/stable-diffusion-inpainting",
+        default="stabilityai/stable-diffusion-2-inpainting",
         help=(
             "The path to the base model to use for evaluation. This can be a local path or a model identifier from the Model Hub."
         ),
